22dc28696000dfef927d193304c10381d6d4a9da
QuevemCosta <quevemcostadacruz@gmail.com>
2020-01-30 23:34:13 -0300

    reformulando carrocel

--- js/carrocel.js
+++ js/carrocel.js
@@ -1,3 +1,38 @@
-function novoItem(){
-    alert("funciona");
-}
+setInterval(
+    function (){
+        let figure = document.querySelectorAll('figure')
+        
+        let setado = document.querySelector('.set')
+
+        switch(setado.id){
+            case 'item-item-1':
+                document.getElementById('item-item-1').className="unset"
+                document.getElementById('item-item-4').className="set"
+                document.getElementById('item-item-3').className="unset"
+                document.getElementById('item-item-2').className="unset"
+                break;
+
+            case 'item-item-4':
+                document.getElementById('item-item-1').className="unset"
+                document.getElementById('item-item-3').className="set"
+                document.getElementById('item-item-4').className="unset"
+                document.getElementById('item-item-2').className="unset"
+                break;
+        
+            case 'item-item-3':
+                document.getElementById('item-item-1').className="unset"
+                document.getElementById('item-item-2').className="set"
+                document.getElementById('item-item-3').className="unset"
+                document.getElementById('item-item-4').className="unset";
+                break;
+            case 'item-item-2':
+                document.getElementById('item-item-2').className="unset"
+                document.getElementById('item-item-1').className="set"
+                document.getElementById('item-item-3').className="unset"
+                document.getElementById('item-item-4').className="unset";
+                break;
+                    
+                
+        }
+    },5000
+)
